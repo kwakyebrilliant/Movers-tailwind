ce9f92586790bb77b6e60e2c282e9ab4ab76ef15
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-23 07:50:08 +0000

    banner updates

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='w-full h-screen p-20 bg-zinc-200 flex flex-col justify-between'>
+        
+    </div>
+  )
+}
+
+export default Banner

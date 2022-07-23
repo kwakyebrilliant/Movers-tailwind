e074f918616e765eb208a3fbb2869f59fc2dfec0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-23 07:03:39 +0000

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
+import { FaHome } from 'react-icons/fa'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-screen h-[70px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='flex items-center'>
+                <FaHome className='sm:w-[300px] text-green-800 lg:w-[35px] lg:h-[35px]' />
+                <h1 className='text-1xl font-bold mr-4 sm:text-4xl'>MOVERS.</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 

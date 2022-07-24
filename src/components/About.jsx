31b36f417b7a6498533c8d3311c274e351d22f86
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-24 14:31:46 +0000

    about updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full my-32'>
+        <div className='max-w-[1240px] mx-auto'>
+            
+        </div>
+    </div>
   )
 }
 

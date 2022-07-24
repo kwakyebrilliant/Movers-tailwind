ec22efeed1324e4fc2ae4d57f7c7565360fe36b9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-24 17:15:54 +0000

    about updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import {
+    ArrowRightIcon,
+} from '@heroicons/react/solid'
+
 const About = () => {
   return (
     <div className='w-full my-32'>
@@ -11,6 +15,57 @@ const About = () => {
             </div>
 
             <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
+
+            <div className="flex flex-col justify-between p-8 transition-shadow bg-white rounded-sm shadow-xl group hover:shadow-lg">
+                <div>
+                    <h5 className="text-5xl font-bold text-green-700">100%</h5>
+                    <div className="pt-2 mt-4 border-t-2 border-indigo-100">
+                    <p className="text-sm font-medium tracking-widest text-gray-500 uppercase">Guaranteed Success</p>
+                    </div>
+                </div>
+
+                <div className="inline-flex items-center mt-16 text-green-700">
+                    <p className="text-lg font-medium">How we do it</p>
+
+                    <ArrowRightIcon className='w-6 h-6 ml-3 transition-transform transform group-hover:translate-x-3' />
+                </div>
+            </div>
+
+            
+            <div className="flex flex-col justify-between p-8 transition-shadow bg-white rounded-sm shadow-xl group hover:shadow-lg">
+                <div>
+                    <h5 className="text-5xl font-bold text-green-700">24/7</h5>
+                    <div className="pt-2 mt-4 border-t-2 border-indigo-100">
+                    <p className="text-sm font-medium tracking-widest text-gray-500 uppercase">Everytime Delivery</p>
+                    </div>
+                </div>
+
+                <div className="inline-flex items-center mt-16 text-green-700">
+                    <p className="text-lg font-medium">How we do it</p>
+
+                    <ArrowRightIcon className='w-6 h-6 ml-3 transition-transform transform group-hover:translate-x-3' />
+                </div>
+            </div>
+
+            <div className="flex flex-col justify-between p-8 transition-shadow bg-white rounded-sm shadow-xl group hover:shadow-lg">
+                <div>
+                    <h5 className="text-5xl font-bold text-green-700">100K</h5>
+                    <div className="pt-2 mt-4 border-t-2 border-indigo-100">
+                    <p className="text-sm font-medium tracking-widest text-gray-500 uppercase">Successful Transaction</p>
+                    </div>
+                </div>
+
+                <div className="inline-flex items-center mt-16 text-green-700">
+                    <p className="text-lg font-medium">How we do it</p>
+
+                    <ArrowRightIcon className='w-6 h-6 ml-3 transition-transform transform group-hover:translate-x-3' />
+                </div>
+            </div>
+
+
+</div>
+
+            {/* <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
                 <div className='border py-8 rounded-xl shadow-xl' >
                     <p className='text-6xl font-bold text-indigo-600'>100%</p>
                     <p className='text-gray-400 mt-2'>Completion</p>
@@ -23,7 +78,7 @@ const About = () => {
                     <p className='text-6xl font-bold text-indigo-600'>100K</p>
                     <p className='text-gray-400 mt-2'>Transactions</p>
                 </div>
-            </div>
+            </div> */}
 
         </div>
     </div>

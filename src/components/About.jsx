3664e86b1d55fd709ccbb56d5825aff8739bb8c5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-24 16:22:46 +0000

    about updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div className='w-full my-32'>
         <div className='max-w-[1240px] mx-auto'>
-            
+            <div className='text-center'>
+            <h2 className='text-5xl font-bold'>Trusted by properties seekers across the world</h2>
+            </div>
         </div>
     </div>
   )

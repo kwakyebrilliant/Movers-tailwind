20033d7c5b9b13035dafa7bc855465b3fe6475a9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-24 19:58:30 +0000

    support updates

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -26,25 +26,97 @@ const support = () => {
 
             <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
 
-            <div className="relative block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
+            <div className="relative hover:shadow-lg block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
                     <span
                         className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
                     ></span>
 
                     <div className="justify-between sm:flex">
                         <div>
-                        <h5 className="text-3xl font-bold text-gray-900">
+                        <h5 className="text-2xl font-bold text-gray-900">
                             Sales
                         </h5>
                         <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
                         </div>
 
                         <div className="flex-shrink-0 hidden ml-3 sm:block">
-                        <img
-                            className="object-cover w-16 h-16 rounded-lg shadow-sm"
-                            src="https://www.hyperui.dev/photos/man-5.jpeg"
-                            alt=""
-                        />
+                        <PhoneIcon className='w-16 h-16 p-4 object-cover bg-green-600 shadow-2xl text-white rounded-lg' />
+                        </div>
+                    </div>
+
+                    <div className="mt-4 sm:pr-8">
+                        <p className="text-sm text-gray-500">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit illum
+                        provident a, ipsa maiores deleniti consectetur nobis et eaque.
+                        </p>
+                    </div>
+
+                    <dl className="flex mt-6">
+                        <div className="flex flex-col-reverse">
+                        <dt className="text-sm font-medium text-gray-600">Published</dt>
+                        <dd className="text-xs text-gray-500">31st June, 2021</dd>
+                        </div>
+
+                        <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                        <dt className="text-sm font-medium text-gray-600">Reading time</dt>
+                        <dd className="text-xs text-gray-500">3 minute</dd>
+                        </div>
+                    </dl>
+                    </div>
+
+                    <div className="relative hover:shadow-lg block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
+                    <span
+                        className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+                    ></span>
+
+                    <div className="justify-between sm:flex">
+                        <div>
+                        <h5 className="text-2xl font-bold text-gray-900">
+                        Technical Support
+                        </h5>
+                        <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+                        </div>
+
+                        <div className="flex-shrink-0 hidden ml-3 sm:block">
+                        <SupportIcon className='w-16 h-16 p-4 object-cover bg-green-600 shadow-2xl text-white rounded-lg' />
+                        </div>
+                    </div>
+
+                    <div className="mt-4 sm:pr-8">
+                        <p className="text-sm text-gray-500">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit illum
+                        provident a, ipsa maiores deleniti consectetur nobis et eaque.
+                        </p>
+                    </div>
+
+                    <dl className="flex mt-6">
+                        <div className="flex flex-col-reverse">
+                        <dt className="text-sm font-medium text-gray-600">Published</dt>
+                        <dd className="text-xs text-gray-500">31st June, 2021</dd>
+                        </div>
+
+                        <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                        <dt className="text-sm font-medium text-gray-600">Reading time</dt>
+                        <dd className="text-xs text-gray-500">3 minute</dd>
+                        </div>
+                    </dl>
+                    </div>
+
+                    <div className="relative hover:shadow-lg block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
+                    <span
+                        className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+                    ></span>
+
+                    <div className="justify-between sm:flex">
+                        <div>
+                        <h5 className="text-2xl font-bold text-gray-900">
+                            Media Inquiries
+                        </h5>
+                        <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+                        </div>
+
+                        <div className="flex-shrink-0 hidden ml-3 sm:block">
+                        <ChipIcon className='w-16 h-16 p-4 object-cover bg-green-600 shadow-2xl text-white rounded-lg' />
                         </div>
                     </div>
 
@@ -69,10 +141,7 @@ const support = () => {
                     </div>
 
                     
-            </div>
-
-            
-            
+            </div>    
 
         </div>
         

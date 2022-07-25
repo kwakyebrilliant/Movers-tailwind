d7549c1d9bd209716c5f9e014e93d6541130aa52
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-25 20:56:03 +0000

    properties updates

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -3,8 +3,35 @@ import Footer from './Footer'
 
 const Properties = () => {
   return (
-    <div className='w-full h-screen pt-20 mx-4'>
-      <h1>hi</h1>
+    <div className='w-full h-screen pt-20'>
+
+      <div className='mx-4 my-8'>
+          <h2 className='text-3xl font-bold'>Categories</h2>
+      </div>
+      
+            <div className='grid md:grid-cols-4 mx-6 gap-8 px-2 py-4 text-center'>
+
+                <div className='border bg-green-800 py-8 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-white'>100%</p>
+                    <p className='text-white mt-2'>Home</p>
+                </div>
+
+                <div  className='border py-8 bg-blue-800 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-white'>24/7</p>
+                    <p className='text-white mt-2'>Offices</p>
+                </div>
+
+                <div className='border py-8 bg-red-700 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-white'>100K</p>
+                    <p className='text-white mt-2'>Luxury</p>
+                </div>
+
+                <div className='border py-8 bg-yellow-700 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-white'>100K</p>
+                    <p className='text-white mt-2'>Industrial</p>
+                </div>
+
+            </div>
 
       <Footer />
     </div>

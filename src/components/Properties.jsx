38ec7161ad6b3363ca477f3d5972d3475c57f40d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-25 09:47:45 +0000

    properties update

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Footer from './Footer'
 
 const Properties = () => {
   return (
     <div className='w-full h-screen pt-20 mx-4'>
       <h1>hi</h1>
+
+      <Footer />
     </div>
   )
 }

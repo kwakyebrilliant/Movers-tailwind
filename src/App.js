34292319f24dcaec6b7ad653ad17e374a1f8262c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-25 08:01:30 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import Support from './components/Support';
-import About from './components/About';
-import Banner from './components/Banner';
+// import Support from './components/Support';
+// import About from './components/About';
+// import Banner from './components/Banner';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Banner />
+      <Home />
+      {/* <Banner />
       <About />
       <Support />
-      <Footer />
+      <Footer /> */}
+      
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Support from '../components/Support';
+import About from '../components/About';
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+        <Banner />
+      <About />
+      <Support />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home

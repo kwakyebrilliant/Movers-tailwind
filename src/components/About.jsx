615559b4009d686633a3d78d3736708c3398071c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-25 06:42:45 +0000

    about updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import {
 
 const About = () => {
   return (
-    <div className='w-full my-32'>
+    <div name="about" className='w-full my-32'>
         <div className='max-w-[1240px] mx-auto'>
 
             <div className='text-center'>

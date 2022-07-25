08c5f32bc39db8803e2b4e6732d6dcdd09d1c0ec
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-25 06:30:16 +0000

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Support from './components/Support';
 import About from './components/About';
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <About />
       <Support />
+      <Footer />
     </div>
   );
 }

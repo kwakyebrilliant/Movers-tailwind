6ca57ada8b70102b6a6040655c314a708fa503d1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-26 09:23:22 +0000

    properties updates

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -73,7 +73,7 @@ const Properties = () => {
            <div className='w-full my-32'>
 
           <div className='mx-4 my-8'>
-            <h2 className='text-3xl font-bold'>Flash Deals</h2>
+            <h2 className='text-3xl font-bold'>Hot Deal</h2>
           </div>
 
             <div className='grid md:grid-cols-2 gap-4 mx-8 text-center'>
@@ -83,21 +83,83 @@ const Properties = () => {
             </div>
 
 
-            <div className="flex flex-col justify-between p-8 transition-shadow bg-white rounded-sm shadow-xl group hover:shadow-lg">
-                <div>
-                    <h5 className="text-5xl font-bold text-green-700">24/7</h5>
+            <div className="flex flex-col bg-white rounded-sm">
+             
+                    <h5 className="text-5xl mt-20 font-bold text-green-900">$240,000</h5>
                     <div className="pt-2 mt-4 border-t-2 border-indigo-100">
-                    <p className="text-sm font-medium tracking-widest text-gray-500 uppercase">Everytime Delivery</p>
+                    <p className="text-sm font-medium tracking-widest text-gray-500 uppercase">
+                      123 Wallaby Avenue, Park Road
+                    </p>
+
+                    <p className="text-lg inline-flex font-medium">
+                    <dl className="flex items-center mt-6 space-x-8 text-xs">
+                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                            <svg
+                            className="w-4 h-4 text-green-500"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                            >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                            </svg>
+
+                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                            <dt className="text-gray-900">
+                                Parking
+                            </dt>
+
+                            <dd className="font-medium">
+                                2 spaces
+                            </dd>
+                            </div>
+                        </div>
+
+                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                            <svg
+                            className="w-4 h-4 text-green-500"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                            >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+                            </svg>
+
+                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                            <dt className="text-gray-900">
+                                Bathroom
+                            </dt>
+
+                            <dd className="font-medium">
+                                2 rooms
+                            </dd>
+                            </div>
+                        </div>
+
+                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                            <svg
+                            className="w-4 h-4 text-green-500"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                            >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                            </svg>
+
+                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                            <dt className="text-gray-900">
+                                Bedroom
+                            </dt>
+
+                            <dd className="font-medium">
+                                4 rooms
+                            </dd>
+                            </div>
+                        </div>
+                        </dl>
+                    </p>
+
                     </div>
-                </div>
-
-                <div className="inline-flex items-center mt-16 text-green-700">
-                    <p className="text-lg font-medium">How we do it</p>
-
-                    <HomeIcon className='w-6 h-6 ml-3 transition-transform transform group-hover:translate-x-3' />
-                </div>
+                    <div className="items-center mt-4">
+                    <div className='inline-flex flex-col'>
+                      <button className='px-3 py-2'>View Details</button>
+                      </div>
+                     </div>
+                    
             </div>
-
 
 
             </div>

9edde2fb5a7fb35a8f4494253275707b115a1e68
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-29 11:42:27 +0000

    dashboard updates

--- src/Dashboard/Partial/Layout.jsx
+++ src/Dashboard/Partial/Layout.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import PartialNavbar from './PartialNavbar'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+    <div className='flex flex-auto'>
+        {/* <Sidebar /> */}
+        <div className='grow'>
+            <PartialNavbar />
+            {/* <div className='m-5'>{children}</div> */}
+        </div>
+    </div>
+</>
   )
 }
 
---
+++ src/Dashboard/Partial/PartialNavbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PartialNavbar = () => {
+  return (
+    <nav className='bg-white shadow-sm border-gray-200 mx-2 px-2 py-2.5 rounded'>
+            <div className='container flex justify-between items-center mx-auto'>
+                <div className='flex items-center mx-auto'>
+                    <span className='text-xl font-medium whitespace-nowrap'>
+                        Welcome
+                    </span>
+                </div>
+
+                <div className='flex justify-end pr-4 pt-4'>
+                    {/* <Toggle /> */}
+                </div>
+            </div>
+        </nav>
+  )
+}
+
+export default PartialNavbar

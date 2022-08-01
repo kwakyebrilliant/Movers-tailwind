6889454ceb3dff559da6c08ee8db085bde0839ea
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-01 21:27:10 +0000

    dashboard updates

--- src/Dashboard/Pages/ChatSingle.jsx
+++ src/Dashboard/Pages/ChatSingle.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import PartialNavbar from '../Partial/PartialNavbar'
+import Sidebar from '../Partial/Sidebar'
+
+import { FaArrowLeft } from 'react-icons/fa'
+
+import { useNavigate } from 'react-router-dom';
+
 
 const ChatSingle = () => {
-  return (
-    <div>ChatSingle</div>
-  )
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='text-black'>
+        <>
+    <div className='flex flex-auto'>
+            <Sidebar />
+       <div className='grow'>
+           <PartialNavbar />
+
+           <div className='m-5'>
+
+            <div className='flex my-8'>
+                <FaArrowLeft className="text-gray-500 text-2xl" onClick={() => navigate(-1)} />
+                <p className='mx-2 text-gray-500'>Back</p>
+            </div>
+           
+
+
+           </div>
+
+       </div>
+    </div>
+    </>
+    </div>
+      )
 }
 
 export default ChatSingle

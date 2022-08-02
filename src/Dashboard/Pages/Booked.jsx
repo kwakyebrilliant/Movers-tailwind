09aa4e35f49eb9a88cea49eca0e675ce5ce55750
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-02 13:49:21 +0000

    dashboard updates

--- src/Dashboard/Pages/Booked.jsx
+++ src/Dashboard/Pages/Booked.jsx
@@ -10,9 +10,27 @@ const Booked = () => {
    <Sidebar />
    <div className='grow'>
        <PartialNavbar />
+       
        <div className='m-5'>
-           <p>Booked</p>
+       <header className="rounded-2xl inset-x-0 bottom-0 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+            <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+                <div className="sm:justify-between sm:items-center sm:flex">
+                <div className="text-center sm:text-left">
+                    <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                    Welcome Back, Brilliant!
+                    </h1>
+
+                    <p className="mt-1.5 text-sm text-gray-900">
+                    Check your booked properties on MOVERS 🎉
+                    </p>
+                </div>
+
+                
+                </div>
+            </div>
+            </header>
        </div>
+
    </div>
 </div>
 </>

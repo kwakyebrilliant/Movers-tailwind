1d78aba036f8315d5cdb1c81c3a664dabe39a8e7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-02 14:42:01 +0000

    dashboard updates

--- src/Dashboard/Pages/Booked.jsx
+++ src/Dashboard/Pages/Booked.jsx
@@ -56,7 +56,7 @@ const Booked = () => {
                         <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Country</span>
                         German
                     </td>
-                    <td class="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static">
+                    <td className="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static">
                         <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Status</span>
                         <span className="rounded bg-red-400 py-1 px-3 text-xs font-bold">deleted</span>
                     </td>

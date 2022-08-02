ea0115022ab1539db3b3d9b3482feec3c3d964fa
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-02 15:24:24 +0000

    dashboard updates

--- src/Dashboard/Pages/Booked.jsx
+++ src/Dashboard/Pages/Booked.jsx
@@ -3,6 +3,7 @@ import PartialNavbar from '../Partial/PartialNavbar'
 import Sidebar from '../Partial/Sidebar'
 
 const Booked = () => {
+  const [showModal, setShowModal] = React.useState(false);
   return (
     <div className='text-black'>
     <>
@@ -36,6 +37,58 @@ const Booked = () => {
           <input className=' mt-12 p-2 rounded-md mx-4' type="text" placeholder='Filter Booked..'/>
         </form>
       
+        {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-full my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 dark:bg-zinc-300  rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Property's Details
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <div className="my-4 text-slate-500 text-lg leading-relaxed">
+
+                  <input type="text" id="text" className="border border-green-700 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-zinc-300 dark:border-gray-500 dark:placeholder-gray-900 dark:text-gray-900" placeholder="search..." required/>
+                  
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    Submit
+                  </button>
+                  <button
+                    className="text-red-700 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
       
         <table className="border-collapse w-full">
             <thead>
@@ -62,7 +115,7 @@ const Booked = () => {
                     </td>
                     <td className="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static">
                         <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">View</span>
-                        <a href="/#" className="text-green-600 hover:text-blue-600 pl-6">View Details</a>
+                        <a className="text-green-600 hover:text-blue-600 pl-6" onClick={() => setShowModal(true)}>View Details</a>
                     </td>
                 </tr>
                 <tr className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">

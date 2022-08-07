8a1372e7952cbcdccd9636fda186e73b9d0fc354
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-07 22:48:00 +0000

    dashboard updates

--- src/Dashboard/Pages/AdminPropertiesDetail.jsx
+++ src/Dashboard/Pages/AdminPropertiesDetail.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import PartialNavbar from '../Partial/PartialNavbar'
+import Sidebar from '../Partial/Sidebar'
+
+import { FaArrowLeft } from 'react-icons/fa'
+
+import { useNavigate } from 'react-router-dom';
 
 const AdminPropertiesDetail = () => {
+
+    const navigate = useNavigate();
+
   return (
     <div>AdminPropertiesDetail</div>
   )

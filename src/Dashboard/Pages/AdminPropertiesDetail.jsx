33372b172c7eaeff77eaffd713943a094bc996fb
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-08 13:43:55 +0000

    dashboard updates

--- src/Dashboard/Pages/AdminPropertiesDetail.jsx
+++ src/Dashboard/Pages/AdminPropertiesDetail.jsx
@@ -25,12 +25,15 @@ const AdminPropertiesDetail = () => {
                 <p className='mx-2 text-gray-500'>Back</p>
             </div>
            
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+            {/* <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
             <h3 className="text-3xl font-semibold">
                     Property's Details: P0001
             </h3>
-            </div>
+            </div> */}
 
+            <div className='mb-5'>
+                <img className="object-cover w-full h-80 rounded-2xl" src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
+            </div>
 
 
            </div>

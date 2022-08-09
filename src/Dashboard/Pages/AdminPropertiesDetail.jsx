6bb09a0ea5f8835896888d4679502fffd7560109
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-09 17:01:01 +0000

    dashboard updates

--- src/Dashboard/Pages/AdminPropertiesDetail.jsx
+++ src/Dashboard/Pages/AdminPropertiesDetail.jsx
@@ -34,13 +34,13 @@ const AdminPropertiesDetail = () => {
 
         <div>
             <h1 className='text-gray-500 text-3xl'>Property Location:</h1>
-            <p className='text-black mb-5 font-bold'>123 Wallaby Avenue, Park Road</p>
+            <p className='text-black mb-5'>123 Wallaby Avenue, Park Road</p>
 
             <h1 className='text-gray-500 text-3xl'>Property Space:</h1>
-            <p className='text-black mb-5 font-bold'>123 Wallaby Avenue, Park Road</p>
+            <p className='text-black mb-5'>123 Wallaby Avenue, Park Road</p>
 
             <h1 className='text-gray-500 text-3xl'>Property Parking Space:</h1>
-            <p className='text-black mb-5 font-bold'>1 Space</p>
+            <p className='text-black mb-5'>1 Space</p>
 
         </div>
 

c9ff67a2b0361a4c593cb07d3cbf70cfbd800901
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-14 10:06:30 +0000

    dashboard updates

--- src/Dashboard/Pages/ChatSingle.jsx
+++ src/Dashboard/Pages/ChatSingle.jsx
@@ -4,12 +4,30 @@ import Sidebar from '../Partial/Sidebar'
 
 import { FaArrowLeft } from 'react-icons/fa'
 
+import { AiOutlineSend } from 'react-icons/ai'
+
 import { useNavigate } from 'react-router-dom';
 
 
 const ChatSingle = () => {
 
     const navigate = useNavigate();
+
+    // function difference(date1, date2) {  
+    //     const date1utc = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    //     const date2utc = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+    //     const day = 1000*60*60*24;
+    //     return(date2utc - date1utc)/day
+    //   }
+      
+    //   const date1 = new Date("2020-12-10"),
+    //         date2 = new Date("2021-10-31"),
+    //         time_difference = difference(date1,date2)
+      
+    //   console.log(time_difference)
+
+
+
 
     return (
         <div className='text-black'>
@@ -29,6 +47,32 @@ const ChatSingle = () => {
 
 
            </div>
+
+           <div className="row">
+      <main
+        role="main"
+        className="col-lg-12 ml-auto mr-auto"
+        style={{ maxWidth: "700px" }}
+      >
+        <div className="content mr-auto ml-auto">
+          <div className="col-lg-12 p-3 m-auto mb-3">
+            <div className="d-flex fixed-bottom m-3">
+              <input
+                className="rounded-lg outline-none focus:outline-none ring-1 ring-green-400 p-2 text-lg w-full"
+                type="text"
+                as="textarea"
+                rows={1}
+              />
+              <button
+                type="submit"
+              >
+                <AiOutlineSend className='bg-green-600' size={40} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
 
        </div>
     </div>

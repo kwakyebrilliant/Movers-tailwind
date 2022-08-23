2b6704ce6c2be32e3e7e3c80031ce18272e08a97
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-23 11:59:29 +0000

    admin partial updates

--- src/Admin/Partial/AdminLayout.jsx
+++ src/Admin/Partial/AdminLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PartialNavbar from './PartialNavbar'
-import Sidebar from './Sidebar'
+import AdminSidebar from './AdminSidebar'
 
 const Layout = ({ children }) => {
   return (
     <>
     <div className='flex flex-auto'>
-        <Sidebar />
+        <AdminSidebar />
         <div className='grow'>
             <PartialNavbar />
             <div className='m-5'>{children}</div>

4af2b92c2f57c2870135e2203f178dbdd5974fc4
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-24 17:28:30 +0000

    admin pages updates

--- src/Admin/Pages/AdminProperties.jsx
+++ src/Admin/Pages/AdminProperties.jsx
@@ -31,6 +31,30 @@ const AdminProperties = () => {
             </header>
        </div>
 
+       <div className='justify-center flex'>
+       <a
+                className="inline-flex items-center px-8 py-3 mt-8 text-white bg-green-600 border border-green-600 rounded hover:bg-transparent hover:text-green-600 active:text-green-500 focus:outline-none focus:ring"
+                href="/adminpropertiesdetail"
+              >
+                <span className="text-sm font-medium">Add Property </span>
+
+                <svg
+                  className="w-5 h-5 ml-3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </a>
+       </div>
+
 
        <section>
         <div className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">

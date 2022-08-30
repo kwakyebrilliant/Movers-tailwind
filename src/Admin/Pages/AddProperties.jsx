8a5394e008059d6248b4e40ecce9b0f05b2cd192
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-08-30 10:11:21 +0000

    admin pages updates

--- src/Admin/Pages/AddProperties.jsx
+++ src/Admin/Pages/AddProperties.jsx
@@ -71,15 +71,16 @@ const AddProperties = () => {
                     placeholder="Enter number of bathroom"
                 />
 
-        </div>
-
-        <div>
             <h1 className=' text-gray-500 text-2xl'>Property Bedroom:</h1>
                 <input
                     className="my-4 rounded-lg outline-none focus:outline-none ring-1 ring-green-400 p-2 text-lg w-full"
                     type="text"
                     placeholder="Enter number of bedroom"
                 />
+
+        </div>
+
+        <div>
 
             <h1 className=' text-gray-500 text-2xl'>Property Category:</h1>
                 <form className='my-4'>

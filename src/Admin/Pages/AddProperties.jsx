506ea756c45def19291123f73bc68d4c68a20dff
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-03 12:52:32 +0000

    admin pages updates

--- src/Admin/Pages/AddProperties.jsx
+++ src/Admin/Pages/AddProperties.jsx
@@ -14,6 +14,22 @@ import Mover from '../../artifacts/contracts/Mover.sol/Mover.json';
 
 const moverAddress = "0x01D620fE224Dae9482013E28E3E3Ccee0f40ab4E";
 
+function getAccessToken () {
+    // If you're just testing, you can paste in a token
+    // and uncomment the following line:
+    // return 'paste-your-token-here'
+  
+    // In a real app, it's better to read an access token from an
+    // environement variable or other configuration that's kept outside of
+    // your code base. For this to work, you need to set the
+    // WEB3STORAGE_TOKEN environment variable before you run your code.
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDE0ZGU4NTUwMjAxMTdENDIyY0IxOTRBREJiZERlOTJGZjBkYzkxNzciLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjIyMDkwNzQ1NjUsIm5hbWUiOiJNb3ZlciJ9.04qLL5Jvrb2ZrrL3Mdpe-zMVmXVGuxroEAl_DwUT97E'
+  }
+  
+  function makeStorageClient () {
+    return new Web3Storage({ token: getAccessToken() })
+  }
+
 
 const AddProperties = () => {
 

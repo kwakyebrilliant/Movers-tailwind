f5b2c7f791bff272037f5d0ae8ff9fead446e018
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-10-17 10:27:26 +0000

    single page updates

--- src/components/SinglePage.jsx
+++ src/components/SinglePage.jsx
@@ -8,7 +8,7 @@ import { useLocation } from 'react-router-dom'
 import { ethers } from 'ethers';
 import Mover from '../artifacts/contracts/Mover.sol/Mover.json';
 
-const moverAddress = "0xe1EC8601A53d06D74eE628D06528C3e43d10C0Ee";
+const moverAddress = "0x64D71775c6FC8e5f5aC9a56F4A976B2f4B39A0A4";
 
 const SinglePage = () => {
 

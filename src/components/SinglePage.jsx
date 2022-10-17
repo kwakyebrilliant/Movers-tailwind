77c21e6ddcc066bb99033bbba016a5dda072e574
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-10-17 09:40:40 +0000

    single page updates

--- src/components/SinglePage.jsx
+++ src/components/SinglePage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Footer from './Footer'
 
 import Navbar from './Navbar';
 
+import { ethers } from 'ethers';
+import Mover from '../artifacts/contracts/Mover.sol/Mover.json';
+
+const moverAddress = "0xe1EC8601A53d06D74eE628D06528C3e43d10C0Ee";
+
 const SinglePage = () => {
+
+    const [currentProperty, setCurrentProperty] = useState([]);
+
+    useEffect(() => {
+        async function fetchPropertyOwner() {
+          // If MetaMask exists
+          if (typeof window.ethereum !== "undefined") {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const contract = new ethers.Contract(
+            moverAddress,
+            Mover.abi,
+            provider
+          );
+          try {
+            const data = await contract.fetchPropertyOwner();
+            console.log("data: ", data);
+            setCurrentProperty(data);
+            for (var i = 1; i <= data; i++) {
+              const currentPropertys = await contract.idPropertyOwner(i);
+              setCurrentProperty((currentProperty) => [...currentProperty, currentPropertys]);
+            }
+          } catch (error) {
+            console.log("Error: ", error);
+          }
+        }
+      }
+      fetchPropertyOwner();
+      }, []);
 
   return (
     <div>
@@ -233,266 +266,96 @@ const SinglePage = () => {
 
                 <div className='grid grid-cols-1 lg:grid-cols-3'>
 
-                <div className="block overflow-hidden mx-4 my-4 rounded-2xl">
-                <img className="object-cover w-full h-56" src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
+                {currentProperty.slice(-3).map((data) => (
+                     <div className="block overflow-hidden mx-4 my-4 rounded-2xl">
+                     <img className="object-cover w-full h-56" src={data.nested.hash} alt="" />
+     
+                      <div className="p-4 bg-gray-900">
+                             <dl>
+                             <div>
+                                 <dt className="sr-only">
+                                 Price
+                                 </dt>
+     
+                                 <dd className="text-sm text-gray-100">
+                                 ETH {data.nested.propertyprice}/ $
+                                 </dd>
+                             </div>
+     
+                             <div>
+                                 <dt className="sr-only">
+                                 Address
+                                 </dt>
+     
+                                 <dd className="font-medium">
+                                 {data.nested.propertylocation}
+                                 </dd>
+                             </div>
+                             </dl>
+     
+                             <dl className="flex items-center mt-6 space-x-8 text-xs">
+                             <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                                 <svg
+                                 className="w-4 h-4 text-green-500"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                 >
+                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                                 </svg>
+     
+                                 <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                                 <dt className="text-gray-100">
+                                     Parking
+                                 </dt>
+     
+                                 <dd className="font-medium">
+                                 {data.nested.propertyparkingspace}  spaces
+                                 </dd>
+                                 </div>
+                             </div>
+     
+                             <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                                 <svg
+                                 className="w-4 h-4 text-green-500"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                 >
+                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+                                 </svg>
+     
+                                 <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                                 <dt className="text-gray-100">
+                                     Bathroom
+                                 </dt>
+     
+                                 <dd className="font-medium">
+                                 {data.nested.propertybathroom}  rooms
+                                 </dd>
+                                 </div>
+                             </div>
+     
+                             <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                                 <svg
+                                 className="w-4 h-4 text-green-500"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                 >
+                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                                 </svg>
+     
+                                 <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                                 <dt className="text-gray-100">
+                                     Bedroom
+                                 </dt>
+     
+                                 <dd className="font-medium">
+                                 {data.nested.propertybedroom} rooms
+                                 </dd>
+                                 </div>
+                             </div>
+                             </dl>
+                         </div>
+                     </div>
+                    ))
+                }
 
-                 <div className="p-4 bg-gray-900">
-                        <dl>
-                        <div>
-                            <dt className="sr-only">
-                            Price
-                            </dt>
-
-                            <dd className="text-sm text-gray-100">
-                            $240,000
-                            </dd>
-                        </div>
-
-                        <div>
-                            <dt className="sr-only">
-                            Address
-                            </dt>
-
-                            <dd className="font-medium">
-                            123 Wallaby Avenue, Park Road
-                            </dd>
-                        </div>
-                        </dl>
-
-                        <dl className="flex items-center mt-6 space-x-8 text-xs">
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Parking
-                            </dt>
-
-                            <dd className="font-medium">
-                                2 spaces
-                            </dd>
-                            </div>
-                        </div>
-
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Bathroom
-                            </dt>
-
-                            <dd className="font-medium">
-                                2 rooms
-                            </dd>
-                            </div>
-                        </div>
-
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Bedroom
-                            </dt>
-
-                            <dd className="font-medium">
-                                4 rooms
-                            </dd>
-                            </div>
-                        </div>
-                        </dl>
-                    </div>
-                </div>
-
-                <div className="block overflow-hidden mx-4 my-4 rounded-2xl">
-                <img className="object-cover w-full h-56" src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
-
-                 <div className="p-4 bg-gray-900">
-                        <dl>
-                        <div>
-                            <dt className="sr-only">
-                            Price
-                            </dt>
-
-                            <dd className="text-sm text-gray-100">
-                            $240,000
-                            </dd>
-                        </div>
-
-                        <div>
-                            <dt className="sr-only">
-                            Address
-                            </dt>
-
-                            <dd className="font-medium">
-                            123 Wallaby Avenue, Park Road
-                            </dd>
-                        </div>
-                        </dl>
-
-                        <dl className="flex items-center mt-6 space-x-8 text-xs">
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Parking
-                            </dt>
-
-                            <dd className="font-medium">
-                                2 spaces
-                            </dd>
-                            </div>
-                        </div>
-
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Bathroom
-                            </dt>
-
-                            <dd className="font-medium">
-                                2 rooms
-                            </dd>
-                            </div>
-                        </div>
-
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Bedroom
-                            </dt>
-
-                            <dd className="font-medium">
-                                4 rooms
-                            </dd>
-                            </div>
-                        </div>
-                        </dl>
-                    </div>
-                </div>
-
-                <div className="block overflow-hidden mx-4 my-4 rounded-2xl">
-                <img className="object-cover w-full h-56" src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
-
-                 <div className="p-4 bg-gray-900">
-                        <dl>
-                        <div>
-                            <dt className="sr-only">
-                            Price
-                            </dt>
-
-                            <dd className="text-sm text-gray-100">
-                            $240,000
-                            </dd>
-                        </div>
-
-                        <div>
-                            <dt className="sr-only">
-                            Address
-                            </dt>
-
-                            <dd className="font-medium">
-                            123 Wallaby Avenue, Park Road
-                            </dd>
-                        </div>
-                        </dl>
-
-                        <dl className="flex items-center mt-6 space-x-8 text-xs">
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Parking
-                            </dt>
-
-                            <dd className="font-medium">
-                                2 spaces
-                            </dd>
-                            </div>
-                        </div>
-
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Bathroom
-                            </dt>
-
-                            <dd className="font-medium">
-                                2 rooms
-                            </dd>
-                            </div>
-                        </div>
-
-                        <div className="sm:inline-flex sm:items-center sm:shrink-0">
-                            <svg
-                            className="w-4 h-4 text-green-500"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                            </svg>
-
-                            <div className="sm:ml-3 mt-1.5 sm:mt-0">
-                            <dt className="text-gray-100">
-                                Bedroom
-                            </dt>
-
-                            <dd className="font-medium">
-                                4 rooms
-                            </dd>
-                            </div>
-                        </div>
-                        </dl>
-                    </div>
-                </div>
 
                 </div>
             </div>

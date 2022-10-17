511e7b821d1a6c0b2a20f0dd3a24cf56e1ac45e7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-10-17 11:09:07 +0000

    components updates

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -14,7 +14,7 @@ import Navbar from './Navbar'
 import { ethers } from 'ethers';
 import Mover from '../artifacts/contracts/Mover.sol/Mover.json';
 
-const moverAddress = "0x64D71775c6FC8e5f5aC9a56F4A976B2f4B39A0A4";
+const moverAddress = "0x02D69CDdeEbE4F717cb77d4074f34c7305F8E32C";
 
 
 const Properties = () => {

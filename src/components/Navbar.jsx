767449597c3bb549ca26eafdd2f2f60d46c3967f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-10-24 07:41:22 +0000

    navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         <div className='hidden md:flex pr-4'>
          
           <button className='px-3 py-3 rounded-full'>
-            <Link to="/summary" >
+            <Link to="/userproperties" >
             <FaUser className='w-6 h-6' />
             </Link>
           </button>

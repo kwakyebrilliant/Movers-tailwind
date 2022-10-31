9ac60427138498f59df4f4bab2801e9df72be2f6
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-10-31 11:36:09 +0000

    add properties updates

--- src/Admin/Pages/AddProperties.jsx
+++ src/Admin/Pages/AddProperties.jsx
@@ -168,21 +168,7 @@ const AddProperties = () => {
                     value={propertydescription}
                 />
            
-
-            <h1 className=' text-gray-500 text-2xl'>Property Price:</h1>
-                <input
-                    className="my-4 rounded-lg outline-none focus:outline-none ring-1 ring-green-400 p-2 text-lg w-full"
-                    type="text"
-                    placeholder="Enter Price in ETH"
-                    onChange={(e) => setPropertyPrice(e.target.value)}
-                    value={propertyprice}
-                />
-
-        </div>
-
-        <div>
-
-            <h1 className=' text-gray-500 text-2xl'>Property Category:</h1>
+             <h1 className=' text-gray-500 text-2xl'>Property Category:</h1>
                 <form className='my-4'>
                     <select onChange={(e) => { setPropertyCategory(e.target.value) }} className="rounded-lg outline-none focus:outline-none ring-1 ring-green-400 p-2 text-lg w-full" >
                           <option value="javascript">Select Category</option>
@@ -203,6 +189,20 @@ const AddProperties = () => {
                           <option value="lease">Lease</option>
                     </select>
                 </form>
+
+            <h1 className=' text-gray-500 text-2xl'>Property Price:</h1>
+                <input
+                    className="my-4 rounded-lg outline-none focus:outline-none ring-1 ring-green-400 p-2 text-lg w-full"
+                    type="text"
+                    placeholder="Enter Price in ETH"
+                    onChange={(e) => setPropertyPrice(e.target.value)}
+                    value={propertyprice}
+                />
+
+
+        </div>
+
+        <div>
 
 
             <h1 className='mt-4 text-gray-500 text-2xl'>Property Documents:</h1>

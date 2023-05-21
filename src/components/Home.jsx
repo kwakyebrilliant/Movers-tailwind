08ca88537cb7b4554b2b8e561f49cbdf56a6f864
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-21 00:32:53 +0000

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom'
 import { FaAd, FaMoneyCheck } from 'react-icons/fa'
